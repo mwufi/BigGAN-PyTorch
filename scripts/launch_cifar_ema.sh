293e72fb43472047a4f8aa73ce826adc0c2c1d99
Zen Tang <zen@wayve.ai>
2020-03-11 18:27:11 -0500

    minor

--- scripts/launch_cifar_ema.sh
+++ scripts/launch_cifar_ema.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1 python train.py \
+CUDA_VISIBLE_DEVICES=4,6 python train.py \
 --shuffle --batch_size 50 --parallel \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
