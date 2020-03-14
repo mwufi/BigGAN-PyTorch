cbb4c3bf3901faf5f6a0a0c439c8b36d3206e956
Zen Tang <zen@wayve.ai>
2020-03-13 23:40:33 -0500

    minor

--- scripts/launch_BigGAN_deep.sh
+++ scripts/launch_BigGAN_deep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py \
+python3 train.py \
 --model BigGANdeep \
 --dataset I128_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 256 \
 --num_G_accumulations 8 --num_D_accumulations 8 \
