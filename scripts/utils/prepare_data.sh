33909a3bc5cd1c7b2a9c14da443e9a279785ff46
Zen Tang <zen@wayve.ai>
2020-03-14 14:07:53 -0500

    minor

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 make_hdf5.py --dataset I128 --batch_size 256 --data_root data --half
+python3 make_hdf5.py --dataset I128 --batch_size 64 --data_root data --half
 python3 calculate_inception_moments.py --dataset I128_hdf5 --data_root data
