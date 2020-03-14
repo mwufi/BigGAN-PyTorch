7eebf74b421b7c33b7c05cb892f2cb3a87d5fe08
Zen Tang <zen@wayve.ai>
2020-03-14 14:21:15 -0500

    minor

--- scripts/launch_BigGAN_bs256x8.sh
+++ scripts/launch_BigGAN_bs256x8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=2,3,6,7 python train.py \
---dataset I128_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 256 --load_in_mem  \
---num_G_accumulations 8 --num_D_accumulations 8 \
+--dataset I128_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 64 --load_in_mem  \
+--num_G_accumulations 32 --num_D_accumulations 32 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
 --G_attn 64 --D_attn 64 \
 --G_nl inplace_relu --D_nl inplace_relu \
