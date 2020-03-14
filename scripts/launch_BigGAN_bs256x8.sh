2e94fa84853c0e9c2436dfe861c3eafff1e0758b
Zen Tang <zen@wayve.ai>
2020-03-13 23:52:44 -0500

    minor

--- scripts/launch_BigGAN_bs256x8.sh
+++ scripts/launch_BigGAN_bs256x8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py \
+CUDA_VISIBLE_DEVICES=2,3,6,7 python train.py \
 --dataset I128_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 256 --load_in_mem  \
 --num_G_accumulations 8 --num_D_accumulations 8 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
